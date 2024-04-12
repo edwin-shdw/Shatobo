54f49af314eb4cb4a0ee51610646db0662022a6e
edwin-shdw <edwin.shdw@gmail.com>
2024-04-12 21:27:21 +0200

    feat: remove credential from footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,10 +4,6 @@ export default function Footer() {
   return(
     <footer className="footer">
       <div className="footer__body">
-        <div className="mbe-1">
-          <span>Developed by </span>
-          <a href="https://github.com/edwin-shdw" target="_blank">edwin-shdw</a>
-        </div>
         <a
           href="https://github.com/edwin-shdw/Shatobo"
           target="_blank"

836ab8a35ae53bcf06dd80c6cbe43b9e466254e9
edwin-shdw <edwin.shdw@gmail.com>
2024-05-01 17:31:55 +0200

    feat: add color to extended features

--- src/contents/spotify.ts
+++ src/contents/spotify.ts
@@ -1,6 +1,7 @@
 import type { PlasmoCSConfig } from 'plasmo';
 import { Message, type MessageResponse } from '~types/message';
 import { elementWithIdDidMount, elementWithSelectorDidMount } from '~utils/element-mount';
+import colors from '~utils/colors';
 
 export const config: PlasmoCSConfig = {
   matches: ['https://open.spotify.com/**'],
@@ -11,6 +12,11 @@ export const config: PlasmoCSConfig = {
   await elementWithSelectorDidMount('.EaTxqhHk6J4ecKHwpY5m');
   document.querySelectorAll<HTMLParagraphElement>('.xt5C47eHPYNiriMJxGnC').forEach(node => {
     node.style.userSelect = 'auto';
+
+    const cssTemplateString = `.xt5C47eHPYNiriMJxGnC::selection {background: ${colors.primary}; color: #000}`;
+    const styleTag = document.createElement('style');
+    styleTag.innerHTML = cssTemplateString;
+    document.head.insertAdjacentElement('beforeend', styleTag);
   });
 })();
 
@@ -29,6 +35,7 @@ async function appendLinkToContextMenu(title: string, href: string) {
   // add spotify's list item styling
   link.classList.add('mWj8N7D_OlsbDgtQx5GW', 'vvMORUKtQKpQcpQ992bR');
   link.innerText = title;
+  link.style.color = colors.primary;
   item.appendChild(link);
   contexMenuList.appendChild(item);
 }
